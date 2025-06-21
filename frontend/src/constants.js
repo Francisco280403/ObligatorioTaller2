13fe439232d7cf283706127addc0619ceadf0eea
jkalicha <166032860+jkalicha@users.noreply.github.com>
2025-06-20 21:22:08 -0300

    Checkpoint v2

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,7 +1,7 @@
 import daoAbi from './artifacts/DaoGovernance.json';
 import tokenAbi from './artifacts/VotingToken.json';
 
-export const API_BASE = "http://localhost:5000";
+export const API_BASE = "http://localhost:3000";
 
 export const DAO_ADDRESS = process.env.REACT_APP_DAO_CONTRACT_ADDRESS;
 export const TOKEN_ADDRESS = process.env.REACT_APP_VOTING_TOKEN_ADDRESS;
